148dc4b0087de01c4058b7d798be492ff20d0c40
dwilliams304 <d.williams0434@gmail.com>
2024-04-04 17:59:23 -0700

    Make things easier to look at (more temp styling!)

--- frontend/src/components/common/JobPost.jsx
+++ frontend/src/components/common/JobPost.jsx
@@ -3,31 +3,39 @@ import TestLogo from "../../assets/react.svg";
 
 export default function JobPost({job}){
     return(
-        <div style={{border: "1px solid black", margin: "1rem"}}>
-            <div style={{display: "flex"}}>
-                <img src={TestLogo} alt="company logo" />
-                <h2>{job.jobTitle} - {job.companyName}</h2>
+        <div style={{border: "1px solid black", margin: "1rem", padding: "1rem", display: "flex"}}>
+            <div style={{width: "50%"}}>
+                <div style={{display: "flex"}}>
+                    <img src={TestLogo} alt="company logo" />
+                    <h2>{job.jobTitle} - {job.companyName}</h2>
+                </div>
+                <div>
+                    <p>{job.location}</p>
+                </div>
+                <p>{job.shortDescription}</p>
             </div>
-            <div>
-                <p>Location: {job.location}</p>
+
+            <div style={{width: "50%", textAlign: "right"}}>
+                <p>{job.level}</p>
+                <p>Salary: ${job.salary}/yr</p>
+                <p>Average hours: {job.averageHours} {job.averageHours >=35 ? "(FT)" : "(PT)"}</p>
+                <p>Expected hourly: {Math.round((job.salary / 52 / job.averageHours) * 100) / 100}/hr</p>
+                <div style={{display: "flex", justifyContent: "flex-end"}}>
+                    {
+                        job.daysWorked.map((day, i) => {
+                            return (
+                                <span className={day === true ? "active" : "inactive"} key={i}>
+                                    {day === true ? "Y" : "N"}
+                                </span>
+                            )
+                        })
+                    }
+                </div>
+                <button style={{border: "none", backgroundColor: "rgb(90, 31, 255)",
+                color: "white", padding: "0.5rem 2rem", borderRadius: "30px"}}>
+                    Apply -&gt;
+                </button>
             </div>
-            <p>Skill level: {job.level}</p>
-            <p>Salary: ${job.salary}/yr</p>
-            <p>Average hours: {job.averageHours} {job.averageHours >=35 ? "(FT)" : "(PT)"}</p>
-            <p>Expected hourly: {Math.round((job.salary / 52 / job.averageHours) * 100) / 100}/hr</p>
-            <div style={{display: "flex"}}>
-                {
-                    job.daysWorked.map((day, i) => {
-                        return (
-                            <span className={day === true ? "active" : "inactive"} key={i}>
-                                {day === true ? "Y" : "N"}
-                            </span>
-                        )
-                    })
-                }
-            </div>
-            <p>{job.shortDescription}</p>
-            <button>Apply -&gt;</button>
         </div>
     )
 }
