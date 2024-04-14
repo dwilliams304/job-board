859a3d10a9997c3988f8c84e9f3d79e65b00be06
dwilliams304 <d.williams0434@gmail.com>
2024-04-14 14:00:32 -0700

    Some more options and styling

---
+++ README.md
@@ -0,0 +1,4 @@
+# Job Board
+
+This Job Board is a personal project, created using a React frontend, and will use an Express.JS backend!
+This is currently an incomplete project - but will be receiving ongoing updates.
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,9 +1,33 @@
 export default function SearchBar(){
     return (
-        <section style={{display: "flex", justifyContent: "center", margin: "2rem"}}>
-            <input placeholder="Title, keywords, etc..."/>
-            <input placeholder="Location"/>
-            <button>Filters</button>
+        <section style={{display: "flex", flexDirection: "column", 
+        justifyContent: "center", alignItems: "center", margin: "2rem"}}>
+            <div style={{marginBottom: "0.5rem"}}>
+                <input 
+                    placeholder="Title, keywords, etc..."
+                    className="search-bar"
+                />
+                <input 
+                    placeholder="Location or 'Remote'"
+                    className="search-bar"
+                />
+                <button style={{marginLeft: "1rem", height: "30px"}}>Search</button>
+            </div>
+            <div style={{display: "flex"}}>
+                <select>
+                    <option>On-Site</option>
+                    <option>Hybrid</option>
+                    <option>Remote</option>
+                </select>
+                
+                <select>
+                    <option>Past 24 hours</option>
+                    <option>Past 7 days</option>
+                    <option>Past 30 days</option>
+                </select>
+
+                <button>All Filters</button>
+            </div>
         </section>
     )
 }
