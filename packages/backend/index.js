27572c1a85685e630e55de6cc60516ff535fe149
dwilliams304 <d.williams0434@gmail.com>
2024-04-28 12:16:01 -0700

    Combine index.js and server.js to one

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,6 +1,21 @@
-const server = require('./api/server');
+require('dotenv').config();
+const helmet = require('helmet');
+const express = require('express');
+
 const PORT = process.env.PORT || 9000;
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use((err, req, res) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+});
 
 server.listen(PORT, () => {
     console.log(`\n ### SERVER RUNNING ON PORT ${PORT} ### \n`)
-})
+});
