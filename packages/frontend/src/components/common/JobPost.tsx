155c6989eb48019955c9e0d4c44158e2d0d23d50
dwilliams304 <d.williams0434@gmail.com>
2024-05-12 09:56:01 -0700

    apply button hover effects

--- packages/frontend/src/components/common/JobPost.tsx
+++ packages/frontend/src/components/common/JobPost.tsx
@@ -43,8 +43,10 @@ export default function JobPost({job}: JobPostProps){
                         })
                     }
                 </div>
-                <button className="border rounded-3xl bg-blue-700 text-white px-8 py-2 ">
-                    Apply &rarr;
+                <button className="transition border rounded-3xl bg-blue-700 text-white px-8 py-2 
+                hover:bg-white hover:border-blue-700 hover:text-blue-700 duration-300 ease-in-out">
+                    <span>Apply</span> 
+                    <span>&rarr;</span>
                 </button>
             </div>
         </div>
