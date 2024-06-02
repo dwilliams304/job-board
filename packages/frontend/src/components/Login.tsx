79b9c58cbc8e320b1ccb663ea3c2240ffe9e7754
dwilliams304 <d.williams0434@gmail.com>
2024-06-02 15:28:00 -0700

    basic login form structure

--- packages/frontend/src/components/Login.tsx
+++ packages/frontend/src/components/Login.tsx
@@ -3,8 +3,55 @@ import React from 'react';
 
 export default function Login(){
     return(
-        <div>
-            <p>Login page!</p>
-        </div>
+        <section>
+            <div>
+                <h2>Sign in to your account</h2>
+                <form>
+                    <div>
+                        <label htmlFor='email'>Email</label>
+                        <input
+                            type='email'
+                            name='email'
+                            id='email'
+                            placeholder='email@email.com'
+                            className='border border-solid border-black'
+                        />
+                    </div>
+
+                    
+                    <div>
+                        <label htmlFor='password'>Password</label>
+                        <input
+                            type='password'
+                            name='password'
+                            id='password'
+                            placeholder='••••••••'
+                            className='border border-solid border-black'
+                        />
+                    </div>
+
+                    <div>
+                        <div>
+                            <input 
+                                type='checkbox'
+                                name='remember'
+                                id='remember'
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor='remember'>Remember me</label>
+                        </div>
+
+                        <a>Forgot password?</a>
+                    </div>
+
+                    <button type='submit' className='border border-solid border-black'>Sign in</button>
+                    <p>
+                        Don't have an account yet? 
+                        <a> Sign up!</a>
+                    </p>
+                </form>
+            </div>
+        </section>
     )
 }
