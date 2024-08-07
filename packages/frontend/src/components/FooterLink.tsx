c58b799c7bfa14bed6de2f01312628090bc934a4
dwilliams304 <d.williams0434@gmail.com>
2024-08-07 15:40:01 -0400

    wip

--- packages/frontend/src/components/FooterLink.tsx
+++ packages/frontend/src/components/FooterLink.tsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom"
 import { ScrollToTop } from "../helpers"
 
+interface footerLink {
+    linkDestination: string,
+    linkText: string,
+    scrollToTop: boolean
+}
+
 export default function FooterLink(props: any){ //TEMPORARY
     return(
         <li className="hover:underline">
