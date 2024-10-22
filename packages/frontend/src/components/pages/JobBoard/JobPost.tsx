49e98a915139bd2a36889ccc79681c6b7679ce6d
dwilliams304 <d.williams0434@gmail.com>
2024-10-22 10:51:29 -0400

    make button class for more consistency

--- packages/frontend/src/components/pages/JobBoard/JobPost.tsx
+++ packages/frontend/src/components/pages/JobBoard/JobPost.tsx
@@ -1,3 +1,5 @@
+import Button from "../../common/Button";
+
 type JobPostProps = {
     job : {
         jobTitle: string;
@@ -49,11 +51,11 @@ export default function JobPost({job}: JobPostProps){
                 <p>Salary: ${job.salary.toLocaleString()}/yr</p>
                 <p>Avg. hrs./wk: {job.averageHours} {job.averageHours >=35 ? "(FT)" : "(PT)"}</p>
                 <p>Hourly: ${salary}/hr</p>
-                <button className="transition border rounded-3xl bg-blue-700 text-white px-8 py-2 mt-6
-                hover:bg-white hover:border-blue-700 hover:text-blue-700 duration-300 ease-in-out">
-                    <span>Apply</span> 
-                    <span>&rarr;</span>
-                </button>
+                <Button
+                    type="Primary"
+                    text="Apply &rarr;"
+                    function={() => {}}
+                />
             </div>
         </div>
     )
--- packages/frontend/src/components/pages/JobPage/JobPage.tsx
+++ packages/frontend/src/components/pages/JobPage/JobPage.tsx
@@ -1,15 +1,18 @@
+import Button from "../../common/Button"
+
 export default function JobPage(){
     return (
         <div className="flex flex-col w-full flex-grow px-8 mt-12 space-y-8">
             {/* Post Details */}
             <div className="space-y-1 relative">
                 <h2 className="text-3xl font-bold">Technical Support Engineer II</h2>
-                <p className="text-xl">Mid-Level</p>
-                <p>Madison, WI</p>
-                <div className="flex space-x-2 align-middle text-md">
+                <div className="flex flex-row space-x-4">
+                    <p className="">Mid-Level</p>
+                    <p>Full-Time</p>
+                    <p>Madison, WI</p>
                     <p>$84,000/yr</p>
-                    <p>(Full-Time)</p>
                 </div>
+
                 {/* Company Details */}
                 <div className="flex flex-col border-b-2 border-black">
                     <div className="flex space-x-4">
@@ -22,11 +25,13 @@ export default function JobPage(){
                     </div>
                     <a className="hover:underline cursor-pointer font-bold">4.5/5 (1.4k reviews)</a>
                 </div>
-                <button className="absolute right-0 top-0 px-8 py-2 border-2 border-blue-700 rounded-3xl">
-                    Share Job
-                </button>
+                <Button
+                    type="Secondary"
+                    text="Share Job"
+                    function={() => {}}
+                    style={{position: "absolute", right: "0", top: "0"}}
+                />
             </div>
-
 
             {/* Job Description */}
             <div className="w-full px-12">
@@ -36,7 +41,7 @@ export default function JobPage(){
                         <ul className="list-disc px-12">
                             <li>4+ years of experience as a Support Engineer</li>
                             <li>Bachelor's Degree in Computer Science or related</li>
-                            <li>Excellent writtend and verbal communication skills</li>
+                            <li>Excellent written and verbal communication skills</li>
                             <li>Great understanding of Javascript, TypeScript, HTML, and CSS</li>
                         </ul>
                     </div>
@@ -50,12 +55,16 @@ export default function JobPage(){
 
             {/* Apply, Report, Etc */}
             <div className="space-x-4">
-                <button className="px-8 py-2 bg-blue-700 rounded-3xl text-white">
-                    Apply now
-                </button>
-                <button className="px-8 py-2 bg-slate-300">
-                    Report Job
-                </button>
+                <Button
+                    type="Primary"
+                    text="Apply now"
+                    function={() => {}}
+                />
+                <Button
+                    type="Tertiary"
+                    text="Report Job"
+                    function={() => {}}
+                />
             </div>
         </div>
     )
