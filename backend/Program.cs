2a4281dab65dbb61499ca5b6443059cf46b722c3
dwilliams304 <d.williams0434@gmail.com>
2024-11-08 12:08:14 -0500

    start backend switch to MongoDB

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,31 +1,18 @@
 using JobBoardDotnetBackend.Models;
-using Supabase;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var url = Environment.GetEnvironmentVariable("SUPABASE_URL");
-var api = Environment.GetEnvironmentVariable("SUPABASE_API");
 
-var supabaseOptions = new Supabase.SupabaseOptions
-{
-    AutoRefreshToken = true,
-    AutoConnectRealtime = true
-};
 
 // Add services to the container.
 
-if(!(url is null) && !(api is null)){
-    builder.Services.AddScoped<Supabase.Client>(_ =>
-        new Supabase.Client(url, api, supabaseOptions));
-}
-else{
-    Console.WriteLine("No URL or APIKEY found!");
-}
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddAuthorization();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 
 var app = builder.Build();
@@ -36,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapControllers();
 
 
 app.UseHttpsRedirection();
