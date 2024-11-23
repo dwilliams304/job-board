0c89bdbc0f3796f99b47634a330f7052ab75d331
dwilliams304 <d.williams0434@gmail.com>
2024-11-23 09:29:50 -0500

    start rework to data models

---
+++ backend/Models/Common/Location.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace JobBoardDotnetBackend.Models.Common
+{
+    public class Location
+    {
+        [BsonElement("street_address")]
+        public string? StreetAddress { get; set; } = null!;
+        [BsonElement("street_address_two")]
+        public string? StreetAddressTwo { get; set; } = null!;
+        [BsonElement("city")]
+        public required string City { get; set; }
+        [BsonElement("postal_code")]
+        public required string PostalCode { get; set; }
+        [BsonElement("state")]
+        public required string State { get; set; }
+        [BsonElement("country")]
+        public required string Country { get; set; }
+        [BsonElement("location_type")]
+        public string? LocationType { get; set; } = null!;
+    }
+}
--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -1,26 +1,47 @@
+using JobBoardDotnetBackend.Models.Common;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace JobBoardDotnetBackend.Models
 {
+    public class ProfileDetails
+    {
+        [BsonElement("name")]
+        public required string Name {  get; set; }
+        [BsonElement("location")]
+        public Location? Location { get; set; } = null!;
+        [BsonElement("contact_email")]
+        public string? ContactEmail { get; set; } = null!;
+        [BsonElement("contact_phone")]
+        public string? ContactPhone { get; set; } = null!;
+        [BsonElement("img")]
+        public string? Img { get; set; } = null!;
+    }
     public class User 
     {
         [BsonId, BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
 
-        [BsonElement("first_name")]
-        public required string FirstName { get; set; }
-
-        [BsonElement("last_name")]
-        public required string LastName { get; set; }
-
-        [BsonElement("location")]
-        public required string Location { get; set; }
-
         [BsonElement("email")]
         public required string Email { get; set; }
 
-        [BsonElement("phone")]
-        public required string Phone { get; set; }
+        [BsonElement("password")]
+        public required string PasswordHash { get; set; }
+
+        [BsonElement("user_type")]
+        public required string UserType { get; set; } //Company? Job Seeker?
+
+
+
+        [BsonElement("profile_details")]
+        public required ProfileDetails ProfileDetails { get; set; }
+
+
+        //META
+        [BsonElement("created_at")]
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+        [BsonElement("updated_at")]
+        public DateTime? UpdatedAt { get; set; }
+
     }
 }
